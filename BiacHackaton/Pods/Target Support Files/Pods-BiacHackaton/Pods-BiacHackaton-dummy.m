216a636188891f976d7afeaabad5ca7c86d480b8
Rodrigo Lara Ruano <rodrigo@MacBook-Pro-3.local>
2016-06-20 19:18:41 -0500

    ios project and pods

---
+++ BiacHackaton/Pods/Target Support Files/Pods-BiacHackaton/Pods-BiacHackaton-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BiacHackaton : NSObject
+@end
+@implementation PodsDummy_Pods_BiacHackaton
+@end
---
+++ BiacHackaton/Pods/Target Support Files/Pods-BiacHackatonUITests/Pods-BiacHackatonUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BiacHackatonUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BiacHackatonUITests
+@end
