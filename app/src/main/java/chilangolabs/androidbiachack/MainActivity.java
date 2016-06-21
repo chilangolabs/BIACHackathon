363fdd7a07137e82899b583f407835896a1e033d
Sebastian Tellez <sebastian.tellez.o@gmail.com>
2016-06-20 20:10:27 -0500

    add styles

--- app/src/main/java/chilangolabs/androidbiachack/MainActivity.java
+++ app/src/main/java/chilangolabs/androidbiachack/MainActivity.java
@@ -1,13 +1,37 @@
 package chilangolabs.androidbiachack;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final long SPLASH_SCREEN_DELAY = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                init();
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
+    }
+
+    private void init() {
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        finish();
     }
 }
