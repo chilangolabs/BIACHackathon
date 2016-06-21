e67858044db29ed90aca607423ca76218a5a2c30
Rodrigo Moreno <moreno.rdr@gmail.com>
2016-06-21 04:46:40 -0500

    ! Soporte para usar en OpenShift

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ var http = require('http');
 var server;
 
 server = http.createServer(app);
-server.listen(process.env.PORT || 8000);
+server.listen(process.env.OPENSHIFT_NODEJS_PORT || 8000,
+  process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
 server.on('listening', function() {
-  console.log('Server listening on http://localhost:%d', this.address().port);
+  console.log('Server listening on http://%s:%d', this.address().address,
+    this.address().port);
 });
