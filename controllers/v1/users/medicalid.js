d1415ddf711998bbe64b9df317e011baf86d2b25
Rodrigo Moreno <moreno.rdr@gmail.com>
2016-06-21 01:23:09 -0500

    + Enpoint para enviar ficha medica

---
+++ controllers/v1/users/medicalid.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var errors = require('../../../utils/errors');
+var jwt = require('jsonwebtoken');
+var User = require('../../../models/User');
+
+module.exports = function(router) {
+  router.post('/', function(req, res, next) {
+    var accessToken = req.headers['x-access-token'];
+
+    if (!accessToken) {
+      return next(new errors.ClientUnauthorized());
+    }
+
+    var jwtSecret = req.app.kraken.get('jwt:secret');
+    jwt.verify(accessToken, jwtSecret, function(err, decoded) {
+      if (err) {
+        return next(new errors.ClientInvalidToken(err.message));
+      }
+
+      var relation = req.body.relation;
+      var birthday = req.body.birthday;
+      var bloodType = req.body.bloodType;
+      var height = req.body.height;
+      var weight = req.body.weight;
+      var medications = req.body.medications;
+      var medicalConditions = req.body.medicalConditions;
+      var allergies = req.body.allergies;
+      var medicalNotes = req.body.medicalNotes;
+
+      if (!relation || !birthday || !bloodType || !height || !weight ||
+        !medications || !medicalConditions || !allergies || !medicalNotes) {
+        return next(new errors.ClientBadRequest());
+      }
+
+      var candidateMedicalId = {
+        relation: relation,
+        bloodType: bloodType,
+        height: height,
+        weight: weight,
+        medications: medications,
+        medicalConditions: medicalConditions,
+        allergies: allergies,
+        medicalNotes: medicalNotes
+      };
+
+      User.findOneAndUpdate({
+        _id: decoded.id
+      }, {
+        $push: {
+          medicalIds: candidateMedicalId
+        }
+      }, function(err) {
+        if (err) {
+          return next();
+        }
+
+        res.sendStatus(200);
+      });
+    });
+  });
+};
