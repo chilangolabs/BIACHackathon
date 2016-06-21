f952a87595454436a8bc796580e98b22da1a82d7
Rodrigo Moreno <moreno.rdr@gmail.com>
2016-06-21 08:21:56 -0500

    + Endpoint para obtener las fichas medicas

--- controllers/v1/users/medicalid.js
+++ controllers/v1/users/medicalid.js
@@ -5,58 +5,89 @@ var jwt = require('jsonwebtoken');
 var User = require('../../../models/User');
 
 module.exports = function(router) {
-  router.post('/', function(req, res, next) {
-    var accessToken = req.headers['x-access-token'];
+  router.route('/')
+    .get(function(req, res, next) {
+      var accessToken = req.headers['x-access-token'];
 
-    if (!accessToken) {
-      return next(new errors.ClientUnauthorized());
-    }
-
-    var jwtSecret = req.app.kraken.get('jwt:secret');
-    jwt.verify(accessToken, jwtSecret, function(err, decoded) {
-      if (err) {
-        return next(new errors.ClientInvalidToken(err.message));
+      if (!accessToken) {
+        return next(new errors.ClientUnauthorized());
       }
 
-      var relation = req.body.relation;
-      var birthday = req.body.birthday;
-      var bloodType = req.body.bloodType;
-      var height = req.body.height;
-      var weight = req.body.weight;
-      var medications = req.body.medications;
-      var medicalConditions = req.body.medicalConditions;
-      var allergies = req.body.allergies;
-      var medicalNotes = req.body.medicalNotes;
-
-      if (!relation || !birthday || !bloodType || !height || !weight ||
-        !medications || !medicalConditions || !allergies || !medicalNotes) {
-        return next(new errors.ClientBadRequest());
-      }
-
-      var candidateMedicalId = {
-        relation: relation,
-        bloodType: bloodType,
-        height: height,
-        weight: weight,
-        medications: medications,
-        medicalConditions: medicalConditions,
-        allergies: allergies,
-        medicalNotes: medicalNotes
-      };
-
-      User.findOneAndUpdate({
-        _id: decoded.id
-      }, {
-        $push: {
-          medicalIds: candidateMedicalId
-        }
-      }, function(err) {
+      var jwtSecret = req.app.kraken.get('jwt:secret');
+      jwt.verify(accessToken, jwtSecret, function(err, decoded) {
         if (err) {
-          return next();
+          return next(new errors.ClientInvalidToken());
         }
 
-        res.sendStatus(200);
+        User.findOne({
+          _id: decoded.id
+        }, function(err, user) {
+          if (err) {
+            return next(new errors.MongoError(err.errmsg));
+          }
+
+          res.status.json({
+            ok: true,
+            medicalIds: user.medicalIds
+          });
+        });
+      });
+    })
+    .post(function(req, res, next) {
+      var accessToken = req.headers['x-access-token'];
+
+      if (!accessToken) {
+        return next(new errors.ClientUnauthorized());
+      }
+
+      var jwtSecret = req.app.kraken.get('jwt:secret');
+      jwt.verify(accessToken, jwtSecret, function(err, decoded) {
+        if (err) {
+          return next(new errors.ClientInvalidToken(err.message));
+        }
+
+        var relation = req.body.relation;
+        var birthday = req.body.birthday;
+        var bloodType = req.body.bloodType;
+        var height = req.body.height;
+        var weight = req.body.weight;
+        var medications = req.body.medications;
+        var medicalConditions = req.body.medicalConditions;
+        var allergies = req.body.allergies;
+        var medicalNotes = req.body.medicalNotes;
+
+        if (!relation || !birthday || !bloodType || !height || !weight ||
+          !medications || !medicalConditions || !allergies || !medicalNotes) {
+          return next(new errors.ClientBadRequest());
+        }
+
+        var candidateMedicalId = {
+          relation: relation,
+          bloodType: bloodType,
+          height: height,
+          weight: weight,
+          medications: medications,
+          medicalConditions: medicalConditions,
+          allergies: allergies,
+          medicalNotes: medicalNotes
+        };
+
+        User.findOneAndUpdate({
+          _id: decoded.id
+        }, {
+          $push: {
+            medicalIds: candidateMedicalId
+          }
+        }, function(err) {
+          if (err) {
+            return next(new errors.MongoError(err.errmsg));
+          }
+
+          res.status(201).json({
+            ok: true,
+            message: 'Ficha medica enviada correctamente'
+          });
+        });
       });
     });
-  });
 };
